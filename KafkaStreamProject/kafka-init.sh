f330144067c12bbe0d6854abcd984b273acd233a
reut-sorani <reutsorani1@gmail.com>
2025-06-15 18:21:08 +0300

    change totorial

--- KafkaStreamProject/kafka-init.sh
+++ KafkaStreamProject/kafka-init.sh
@@ -6,3 +6,12 @@ kafka-topics.sh \
   --replication-factor 1 \
   --if-not-exists \
   --bootstrap-server localhost:9092
+
+# Create topic: input_zip_topic
+kafka-topics.sh \
+  --create \
+  --topic input_zip_topic \
+  --partitions 3 \
+  --replication-factor 1 \
+  --if-not-exists \
+  --bootstrap-server localhost:9092
